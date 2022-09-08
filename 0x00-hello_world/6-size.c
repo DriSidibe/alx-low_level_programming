49daac2c3f74bcf3634871eb753df342c41f7a9d
DriSidibe <dsidibe653@gmail.com>
2022-09-08 07:12:44 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %d byte(s)",sizeof(char));
-	printf("Size of an int: %d byte(s)",sizeof(int));
-	printf("Size of a long int: %d byte(s)",sizeof(int));
-	printf("Size of a long long int: %d byte(s)",sizeof(long));
-	printf("Size of float: %d byte(s)",sizeof(sizeof(float));
+	printf("Size of char: %ld byte(s)",sizeof(char));
+	printf("Size of an int: %ld byte(s)",sizeof(int));
+	printf("Size of a long int: %ld byte(s)",sizeof(int));
+	printf("Size of a long long int: %ld byte(s)",sizeof(long));
+	printf("Size of float: %ld byte(s)",sizeof(float));
 	return (0);
 }
