1c38247e4805c54836cbaa6afbe00166ab3bbc1b
DriSidibe <dsidibe653@gmail.com>
2022-09-08 05:36:14 -0700

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - the main function
+ *
+ *Return: 0 if the program was runnig successfuly
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
