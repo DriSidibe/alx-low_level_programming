114410e00cd75ec90aa0c6a0866fad3aa9bed29b
DriSidibe <dsidibe653@gmail.com>
2022-09-09 06:53:54 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,11 @@
 
 
 /* betty style doc for function main goes there */
-
+/**
+ *main - the main function
+ *
+ *Return: 0 is success
+ */
 int main(void)
 
 {
@@ -26,11 +30,11 @@ int main(void)
 	}
 	else if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	}
 	else
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	}
 
 	return (0);
