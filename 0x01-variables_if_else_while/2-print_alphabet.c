61685e9923db98a9052be47d36ccfe64a4147e72
DriSidibe <dsidibe653@gmail.com>
2022-09-09 10:29:34 -0700

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+/* betty style doc for function main goes there */
+/**
+ *main - the main function
+ *
+ *Return: 0 is success
+ */
+int main(void)
+
+{
+
+	char a;
+	char end_line[10] = "\n";
+
+	for (a='a'; a<='z' ;a++)
+		putchar(a);
+
+	return (0);
+
+}
