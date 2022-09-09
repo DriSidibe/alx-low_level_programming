52e55432d83930b009c30cf21b732c92d2babb89
DriSidibe <dsidibe653@gmail.com>
2022-09-09 11:33:16 -0700

    commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+/* betty style doc for function main goes there */
+/**
+ *main - the main function
+ *
+ *Return: 0 is success
+ */
+int main(void)
+
+{
+
+	char a;
+
+	for (a = 'z'; a >= 'a' ; a--)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+
+}
