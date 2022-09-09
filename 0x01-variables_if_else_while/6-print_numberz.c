7a58726c157ccd5784b9ec9d91d467ca17a26167
DriSidibe <dsidibe653@gmail.com>
2022-09-09 11:29:48 -0700

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+/* betty style doc for function main goes there */
+/**
+ *main - the main function
+ *
+ *Return: 0 is success
+ */
+int main(void)
+
+{
+
+	int a;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+
+	return (0);
+
+}
