cb06f8d5f1a034a72ad0153ba974fd778accf604
DriSidibe <dsidibe653@gmail.com>
2022-09-09 10:43:34 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+/* betty style doc for function main goes there */
+/**
+ *main - the main function
+ *
+ *Return: 0 is success
+ */
+int main(void)
+
+{
+
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+	{
+		if (c != 'q' || c != 'e')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
