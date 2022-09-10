87856572e7a23c0dfcd154a343b4a56da59ac20e
DriSidibe <dsidibe653@gmail.com>
2022-09-10 01:11:05 -0700

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -38,7 +38,7 @@ int main(void)
 					putchar(' ');
 					putchar(c + '0');
 					putchar(d + '0');
-					if (a != 9 || b != 8 | c != 9 || d != 9)
+					if ((a != 9) || (b != 8) || (c != 9) || (d != 9))
 					{
 						putchar(',');
 						putchar(' ');
