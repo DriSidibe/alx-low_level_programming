3a3c9083ff8e1285fe9645de6e707ff005e3aafa
DriSidibe <dsidibe653@gmail.com>
2022-09-13 07:29:49 -0700

    commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,38 +1,6 @@
-#include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
 #include <ctype.h>
-
-#include <time.h>
-
-/* more headers goes there */
-
-
-/* betty style doc for function main goes there */
-
-/**
- *_isalpha - check the code
- *@c: the parameter
- *
- *Return: a result
- */
-int _isalpha(int c);
-
-/**
- *main - the main function
- *
- *Return: 0 is success
- */
-int main(void)
-
-{
-
-
-	/* your code goes there */
-
-
-	return (0);
-
-}
 
 /**
  *_islower - check the code
