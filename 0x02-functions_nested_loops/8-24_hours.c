7d9fc7d16824dbc4d80168a155cd83d8921d4d42
DriSidibe <dsidibe653@gmail.com>
2022-09-13 08:42:32 -0700

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,15 +8,15 @@
 void jack_bauer()
 {
 	int hour;
-	int minute;
+	char minute;
 
 	for (hour = 0 ; hour < 24 ; hour++)
 	{
-		for (minute = 0 ; minute < 60 ; minute++)
+		for (minute = '0' ; minute < '60' ; minute++)
 		{
 			_putchar(hour + '0');
 			_putchar(':');
-			_putchar(minute + '0');
+			_putchar(minute);
 			_putchar('\n');
 		}
 	}
