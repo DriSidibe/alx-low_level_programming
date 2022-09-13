1c6e40f804ae4a0e3dbb246bbd20788186b689e9
DriSidibe <dsidibe653@gmail.com>
2022-09-13 05:30:06 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,35 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <time.h>
-
-/* more headers goes there */
-
-
-/* betty style doc for function main goes there */
-/**
- *main - the main function
- *
- *Return: 0 is success
- */
-
-void print_alphabet(void);
-
-int main(void)
-
-{
-
-
-	/* your code goes there */
-	print_alphabet();
-
-	return (0);
-
-}
-
-/**
- *print_alphabet - print alphabet in lowercase
- */
 void print_alphabet(void)
 {
 	for (char a = 'a' ; a <= 'z' ; a++)
