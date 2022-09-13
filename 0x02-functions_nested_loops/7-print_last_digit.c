b2ccba346ebf9d1ec8081e610e38c3a296467af6
DriSidibe <dsidibe653@gmail.com>
2022-09-13 08:05:12 -0700

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <math.h>
 #include <ctype.h>
 
 /**
@@ -13,7 +12,7 @@ int print_last_digit(int n)
 
 	int last_digit;
 
-	last_digit = ((abs(n) % 10) * 10) + (abs(n) % 10);
+	last_digit = n % 10;
 
-	return (last_digit);
+	return (last_digit * 10 + last_digit);
 }
