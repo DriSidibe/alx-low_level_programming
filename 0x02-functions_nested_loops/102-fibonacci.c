f5915069c36b58a2c66d2b6eccb3fb9f3cf43bb6
DriSidibe <dsidibe653@gmail.com>
2022-09-13 15:45:57 -0700

    commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - check the code
+ *
+ *Return: check the code
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 0 ; i <= 50 ; i++)
+	{
+		if (i == 0)
+		{
+			printf("1");
+		}
+		else if (i == 2)
+		{
+			printf(", 2");
+		}
+		else
+		{
+			printf(", %d", i);
+		}
+	}
+
+	return (0);
+}
