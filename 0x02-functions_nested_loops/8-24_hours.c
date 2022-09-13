d1d6b43808c8660b42608de154c77d03b5c6014a
DriSidibe <dsidibe653@gmail.com>
2022-09-13 05:20:54 -0700

    commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+/* betty style doc for function main goes there */
+
+/**
+ *_abs - check the code
+ *
+ *Return: a result
+ */
+int _abs(int n);
+
+/**
+ *main - the main function
+ *
+ *Return: 0 is success
+ */
+int main(void)
+
+{
+
+
+	/* your code goes there */
+
+
+	return (0);
+
+}
+
+/**
+ *_abs - check the code
+ *@n: the parameter
+ *
+ *Return: check the code
+ */
+int _abs(int n)
+{
+	return abs(n);
+}
