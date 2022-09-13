19e4368946a53462676daf6bea0867fe5f314cba
DriSidibe <dsidibe653@gmail.com>
2022-09-13 02:19:11 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+/* betty style doc for function main goes there */
+/**
+ *main - the main function
+ *
+ *Return: 0 is success
+ */
+int main(void)
+
+{
+
+
+	/* your code goes there */
+	printf("_putchar");
+
+	return (0);
+
+}
