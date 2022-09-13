9a707d817c73638d22fef9e29dc4eb6523e34aaf
DriSidibe <dsidibe653@gmail.com>
2022-09-13 07:32:12 -0700

    commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,38 +1,6 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 #include <ctype.h>
-
-#include <time.h>
-
-/* more headers goes there */
-
-
-/* betty style doc for function main goes there */
-
-/**
- *print_sign - check the code
- *@n: the parameter
- *
- *Return: a result
- */
-int print_sign(int n);
-
-/**
- *main - the main function
- *
- *Return: 0 is success
- */
-int main(void)
-
-{
-
-
-	/* your code goes there */
-
-
-	return (0);
-
-}
 
 /**
  *print_sign - check the code
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+int print_sign(int n);
 int _isalpha(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
