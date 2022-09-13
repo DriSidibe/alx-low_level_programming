426110bb305903689ec618e8df0d82a6dfbc0687
DriSidibe <dsidibe653@gmail.com>
2022-09-13 12:58:50 -0700

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,17 +12,23 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n < 98)
+	if (n <= 98)
 	{
 		for (i = n ; i < 99 ; i++)
 		{
-			printf("%d", i);
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
 		}
-		printf("99, 98");
 	}
 	else
 	{
-		for (i = n ; i > 99 ; i++)
+		for (i = n ; i > 99 ; i--)
 		{
 			if (i != 98)
 			{
