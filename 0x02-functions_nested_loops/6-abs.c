b8000d10cc73fe69c4c1fcaa96e7adddbfc059dd
DriSidibe <dsidibe653@gmail.com>
2022-09-13 07:35:54 -0700

    commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,37 +1,6 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 #include <ctype.h>
-
-#include <time.h>
-
-/* more headers goes there */
-
-
-/* betty style doc for function main goes there */
-
-/**
- *_abs - check the code
- *
- *Return: a result
- */
-int _abs(int n);
-
-/**
- *main - the main function
- *
- *Return: 0 is success
- */
-int main(void)
-
-{
-
-
-	/* your code goes there */
-
-
-	return (0);
-
-}
 
 /**
  *_abs - check the code
