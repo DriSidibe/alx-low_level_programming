6948a6b446cb1e4f30b34ad153e16eb9658c1334
DriSidibe <dsidibe653@gmail.com>
2022-09-14 16:24:56 -0700

    commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,22 +8,22 @@
 int main(void)
 {
 
-	long long int two_back;
-	long long int one_back;
-	long long int sum;
+	long double two_back;
+	long double one_back;
+	long double sum;
 	int i;
 
 	one_back = 2;
 	two_back = 1;
 
-	printf("%lli, %lli", 1, 2);
+	printf("%Lf, %Lf", 1, 2);
 
 	for (i = 2 ; i < 98 ; i++)
 	{
 		sum = one_back + two_back;
 		two_back = one_back;
 
-		printf(", %lli", sum);
+		printf(", %.0LF", sum);
 
 		one_back = sum;
 	}
