30ca53f78a4c2632d1e02741001a1356b0ee2a59
DriSidibe <dsidibe653@gmail.com>
2022-09-15 00:26:52 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_isupper - check the code
+ *@c: the parameter
+ *
+ *Return: check the code
+ */
+int _isupper(int c)
+{
+
+	return (isupper(c) == 0 ? 0 : 1);
+}
