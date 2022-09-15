571cdf73a0604d7a97d5052c53c37dc8cb0c3a81
DriSidibe <dsidibe653@gmail.com>
2022-09-15 10:23:05 -0700

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,9 @@ void more_numbers(void)
 	int i;
 	int a;
 
-	for (i = 0 ; i <= 14 ; i++)
+	for (i = 0 ; i <= 9 ; i++)
 	{
-		for (a = 48 ; a <= 57 ; a++)
+		for (a = 0 ; a <= 14 ; a++)
 		{
 			if (a > 9)
 			{
