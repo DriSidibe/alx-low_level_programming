fec576397c005b1f13a2bb9ea5b7b24aebdac936
DriSidibe <dsidibe653@gmail.com>
2022-09-15 13:03:18 -0700

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,11 +3,19 @@
 /**
  *print_number - check the code
  *@c: the parameter
- *
- *Return: check the code
  */
 void print_number(int c)
 {
 
-	_putchar(c + '0');
+	unsigned int i = c;
+
+	if (c < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+	if ((i / 10) > 0)
+		print_number(i / 10);
+
+	_putchar((i % 10) + '0');
 }
