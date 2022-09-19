5cc8141e3685669979aa012438cb9f0df33e4609
DriSidibe <dsidibe653@gmail.com>
2022-09-19 14:04:09 -0700

    commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+
+
+/**
+ *generator -  generates random valid passwords for the program 101-crackme.
+ *@len: the lenght of the random password
+ *
+ *Return: the random password
+ */
+char generator(int len)
+{
+
+	char alphanum[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s[20];
+
+	for (int i = 0; i < len; ++i)
+	{
+		s[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+	}
+
+	return s;
+}
+
+
+/**
+ *main - the main function.
+ *
+ *Return: the return code
+ */
 int main(void)
 {
 
+	srand(time(NULL));
+
+	generator(15);
+
+	return 0;
 }
