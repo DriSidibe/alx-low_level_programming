8214bb33400f4071c9618109d885d9c12d5096b5
DriSidibe <dsidibe653@gmail.com>
2022-09-19 03:33:11 -0700

    commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting with the first character, followed by a new line.
+ * puts2 - prints every other character of a string,
+ * starting with the first character, followed by a new line.
  * @str: the string
  */
 void puts2(char *str)
@@ -11,7 +12,7 @@ void puts2(char *str)
 
 	for (i = 0 ; i < strlen(str) ; i++)
 	{
-		if (*(str + i) == 0 || *(str + i) == 2 || *(str + i) == 4 || *(str + i) == 8)
+		if (i % 2 == 0)
 		{
 			_putchar(*(str + i));
 		}
