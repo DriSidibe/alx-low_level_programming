eddd26eb8fe23565877d0f577f89e4cb7c089e10
DriSidibe <dsidibe653@gmail.com>
2022-09-19 02:02:37 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: the string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0 ; i <= strlen(str) ; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+
+}
