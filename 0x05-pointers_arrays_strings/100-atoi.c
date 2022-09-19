9be3767010e44aed885cde7e1b2f21eb23d2221d
DriSidibe <dsidibe653@gmail.com>
2022-09-19 13:22:06 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include <string.h>
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: the string to convert
+ *
+ *Return: return the result
+ */
+int _atoi(char *s);
+{
+	char number_in_string;
+	size_t i;
+
+	for (i = 0 ; i < strlen(s) ; i++)
+	{
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+		{
+			number_in_string
+	}
+
+	return (dest);
+}
