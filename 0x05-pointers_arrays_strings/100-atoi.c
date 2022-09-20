d3a31a19087b76d3619b69c9bc5564dbd8532af0
DriSidibe <dsidibe653@gmail.com>
2022-09-20 09:32:42 -0700

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,43 +10,20 @@
  */
 int _atoi(char *s)
 {
-	int first_digit_pos;
-	int i, j, k, digit_count = 0;
+	int i, k, digit_found = 1;
 
 	for (i = 0 ; i < (int)strlen(s) ; i++)
 	{
 		if ((*(s + i) >= '0' && *(s + i) <= '9') || (*(s + i) == '-' && *(s + i + 1) >= '0' && *(s + i + 1) <= '9'))
 		{
-			first_digit_pos = i;
-			j = i;
-			break;
-		}
-	}
-
-	for (i = j ; i < (int)strlen(s) ; i++)
-	{
-		if ((*(s + i) >= '0' && *(s + i) <= '9') || *(s + i) == '-')
-		{
-			digit_count++;
+			if (digit_found == 0)
+				*(s + i) = ' ';
 		}
 		else
 		{
-			break;
+			digit_found = 0;
+			*(s + i) = ' ';
 		}
-	}
-
-
-	for (k = 0 ; k < first_digit_pos ; k++)
-	{
-		for (i = 0 ; i < (int)strlen(s) - 1 ; i++)
-		{
-			*(s + i) = *(s + i + 1);
-		}
-	}
-
-	for (k = digit_count ; k < (int)strlen(s) ; k++)
-	{
-		*(s + k) = ' ';
 	}
 
 	if (*s == ' ')
