a51f5b5762287cca66f5ce4ab73f3322b477fa6b
DriSidibe <dsidibe653@gmail.com>
2022-09-20 07:47:25 -0700

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -9,6 +10,36 @@
  */
 int _atoi(char *s)
 {
+	int first_digit_pos;
+	char number_in_str;
+	int i, j, k;
+
+	for (i = 0 ; i < (int)strlen(s) ; i++)
+	{
+		if (*(s + i) >= '0' && *(s + i) <= '9')
+		{
+			first_digit_pos = i;
+			j = i;
+			break;
+		}
+	}
+
+	for (i = j ; i < (int)strlen(s) ; i++)
+	{
+		if (!(*(s + i) >= '0' && *(s + i) <= '9'))
+		{
+			*(s + i) = ' ';
+		}
+	}
+
+
+	for (k = 0 ; k < first_digit_pos ; k++)
+	{
+		for (i = 0 ; i < (int)strlen(s) - 1 ; i++)
+		{
+			*(s + i) = *(s + i + 1);
+		}
+	}
 
 	return (atoi(s));
 }
