fd9b45a6eda46155dcc0108b403c49c253bee521
DriSidibe <dsidibe653@gmail.com>
2022-09-21 15:00:14 -0700

    commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,24 +8,19 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int i, j;
+	char b[] = "4433007711";
+	char a[] = "aAeEoOtTlL";
 
-	for (i = 0 ; i < (int)strlen(s) ; i++)
+	for (i = 0; *(s + i); i++)
 	{
-		if (*(s + i) == 'a' || *(s + i) == 'A')
-			*(s + i) = '4';
-
-		else if (*(s + i) == 'e' || *(s + i) == 'E')
-			*(s + i) = '3';
-
-		else if (*(s + i) == 'o' || *(s + i) == 'O')
-			*(s + i) = '0';
-
-		else if (*(s + i) == 't' || *(s + i) == 'T')
-			*(s + i) = '7';
-
-		else if (*(s + i) == 'l' || *(s + i) == 'L')
-			*(s + i) = '1';
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+			}
+		}
 	}
 
 	return (s);
