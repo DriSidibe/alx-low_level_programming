dfdbf401f4b544ef46a197dd78fa17ee3608c2e0
DriSidibe <dsidibe653@gmail.com>
2022-09-21 02:54:33 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: the destination string
+ * @src: the source string.
+ *
+ * Return: the result
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
