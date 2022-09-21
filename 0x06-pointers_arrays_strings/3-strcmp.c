215a67b72c52a37499ce92dc30d3be4768396a62
DriSidibe <dsidibe653@gmail.com>
2022-09-21 08:22:15 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings.
+ * @s1 the destination string
+ * @s2: the source string.
+ *
+ * Return: the result
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
