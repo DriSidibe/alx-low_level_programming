b43f9a27df32d422d25825dc28cc99f7b35b94f4
DriSidibe <dsidibe653@gmail.com>
2022-09-21 03:22:21 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * _strncpy - copy a string in another.
+ * @dest: the destination string
+ * @src: the source string.
+ * @n: use at most n bytes from src
+ * Return: the result
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
