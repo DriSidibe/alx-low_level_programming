0601f639a0827fd195a450267a662f15f063a556
DriSidibe <dsidibe653@gmail.com>
2022-09-22 09:53:49 -0700

    commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,13 +11,19 @@ void print_number(int n)
 	int i;
 	int first_digit;
 	int remain;
+	int r;
 	int number_of_digit = floor(log10(n)) + 1;
 
 	for (i = number_of_digit; i > 0; i--)
 	{
-		remain = n % (int)round(pow((double)10, (double)(i - 1)));
-		first_digit = (n - remain) / (int)round(pow((double)10, (double)(i - 1)));
+		r = round(pow(10, (i - 1)))
+
+		remain = n % r;
+
+		first_digit = (n - remain) / r;
+
 		_putchar('0' + first_digit);
+
 		n -= n - remain;
 	}
 }
