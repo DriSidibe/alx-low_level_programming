192158fe6d811b2b58ae54a7228c892676361264
DriSidibe <dsidibe653@gmail.com>
2022-09-26 02:20:36 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ *_strchr - locates a character in a string.
+ *@s: the string
+ *@c: the charactere
+ *
+ *Return:  a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	return(strchr(s, c));
+
+}
