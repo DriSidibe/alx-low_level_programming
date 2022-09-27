a1b54208975729ca1269d30f8b9cac96f602603f
DriSidibe <dsidibe653@gmail.com>
2022-09-27 03:05:58 -0700

    commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,6 +10,22 @@
  */
 unsigned int *_strspn(char *s, char *accept)
 {
-	return ((unsigned int)strspn(s, accept));
+	int i, j, len = 0, in = 0;
 
+	for (i = 0; i < strlen(s), i++)
+	{
+		for  (j = 0, j < strlen(accept), j++)
+		{
+			if (in != 1)
+			{
+				if (s[i] == accept[j])
+				{
+					in  = 1;
+					len++;
+					break;
+				}
+			}
+		}
+	}
+	return (len);
 }
