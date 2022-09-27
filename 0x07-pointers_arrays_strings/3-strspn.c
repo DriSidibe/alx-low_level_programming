cea9bab68bcc69fc5ed7127a8a78c69195bcb90c
DriSidibe <dsidibe653@gmail.com>
2022-09-27 02:20:54 -0700

    commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,10 +5,11 @@
  *@s: the string
  *@accept: the list of char
  *
- *Return:  a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
+ *Return:  a pointer to the first occurrence
+ *of the character c in the string s, or NULL if the character is not found
  */
 char *_strspn(char *s, char *accept)
 {
-	return(strspn(s, accept));
+	return (strspn(s, accept));
 
 }
