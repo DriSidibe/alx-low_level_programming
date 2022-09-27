de7afe97c842fa13bdf32cc3129ad48e3cfd366b
DriSidibe <dsidibe653@gmail.com>
2022-09-27 02:19:07 -0700

    commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,10 +5,11 @@
  *@s: the string
  *@c: the charactere
  *
- *Return:  a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
+ *Return:  a pointer to the first occurrence
+ *of the character c in the string s, or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
-	return(strchr(s, c));
+	return (strchr(s, c));
 
 }
