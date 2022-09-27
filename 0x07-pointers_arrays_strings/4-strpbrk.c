fbca56142a1c143f5e69c936ec2cac78612b944e
DriSidibe <dsidibe653@gmail.com>
2022-09-27 03:09:59 -0700

    commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,10 +5,11 @@
  *@s: the string
  *@accept: the list of char
  *
- *Return: pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found
+ *Return: pointer to the byte in s that matches one of
+ *the bytes in accept, or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
-	return(strpbrk(s, accept));
+	return (strpbrk(s, accept));
 
 }
