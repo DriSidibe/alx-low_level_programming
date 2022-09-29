d1f7554dcf0bbda2909f72fc6f99af8477473430
DriSidibe <dsidibe653@gmail.com>
2022-09-29 05:23:10 -0700

    commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include <math.h>
+
+/**
+ *_sqrt_recursion - returns the natural square root of a number.
+ *@n: the number
+ *
+ *Return: the natural square root of n
+ */
+int _sqrt_recursion(int n)
+
+{
+
+	if (n - floor(sqrt(n)) < 1)
+	{
+		return (-1);
+	}
+	return (sqrt(n));
+
+}
