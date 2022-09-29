473c19dd919ec5c3444b0cc0f8e97953244228c7
DriSidibe <dsidibe653@gmail.com>
2022-09-29 06:22:36 -0700

    commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,13 +4,27 @@
  *
  *Return: the natural square root of n
  */
+int is_sqrt(int g, int r)
+{
+	if (g * g == r)
+		return (g);
+	if (g * g < r)
+		return (is_sqrt(++g, r));
+	else
+		return (-1);
+}
+
+/**
+ *_sqrt_recursion - returns the natural square root of a number
+ *
+ *@n: numbert to test
+ *
+ *Return: square root of number n
+ */
 int _sqrt_recursion(int n)
 {
-
-	if (n - floor(sqrt(n)) < 1)
-	{
-		return (-1);
-	}
-	return (sqrt(n));
-
+	if (n != 0)
+		return (is_sqrt(0, n));
+	else
+		return (0);
 }
