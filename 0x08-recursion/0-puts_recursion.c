ebfbe5a8f95563dc427e5bb5e70d207b8b2b687b
DriSidibe <dsidibe653@gmail.com>
2022-09-29 00:16:21 -0700

    commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,7 +4,7 @@
  *_puts_recursion - prints a string, followed by a new line.
  *@s: the string to print
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	puts(s);
 	puts("\n");
