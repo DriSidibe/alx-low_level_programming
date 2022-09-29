72510b65be00e52eabf4304cc00cf219b95ce57c
DriSidibe <dsidibe653@gmail.com>
2022-09-29 05:06:23 -0700

    commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ *_pow_recursion - returns the value of x raised to the power of y.
+ *@x: value 1
+ *@y: value 2
+ *
+ *Return: value of x raised to the power of y.
+ */
+int _pow_recursion(int x, int y)
+
+{
+	return (pow(x , y));
+}
