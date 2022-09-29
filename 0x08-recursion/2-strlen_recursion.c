9d003a5aec2ea74881203f02a1879868a94fa98c
DriSidibe <dsidibe653@gmail.com>
2022-09-29 04:31:30 -0700

    commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include <tring.h>
+
+/**
+ *_strlen_recursion - return the lenht of a string
+ *@s: the string
+ *
+ *Return: the lenght of s
+ */
+int _strlen_recursion(char *s)
+
+{
+	return (strlen(s));
+}
