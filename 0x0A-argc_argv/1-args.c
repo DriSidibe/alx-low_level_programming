06fc7b6ec639ae8fa1ccabcd2640e7d61f45cfcb
DriSidibe <dsidibe653@gmail.com>
2022-09-30 01:48:22 -0700

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - prints the number of arguments passed into it.
+ *
+ *REturn: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	puts(argc);
+	puts("\n");
+
+	return (0);
+}
