8970b0762d3d8ffa38e0208cb567c7675efc5355
DriSidibe <dsidibe653@gmail.com>
2022-10-04 04:50:27 -0700

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,20 @@ char *create_array(unsigned int size, char c)
 {
 
 	int i;
-	char *p = malloc(sizeof(char) * size);
+	char *p;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	malloc(sizeof(char) * size);
+
+
+	if (p == NULL)
+	{
+		return (NULL); 
+	}
 
 	for (i = 0 ; i < size ; i++)
 	{
