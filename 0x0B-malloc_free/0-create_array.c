8b18d991961f6d215f7f861a99d046d39951a422
DriSidibe <dsidibe653@gmail.com>
2022-10-04 04:44:14 -0700

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 
 /**
- *create_array - creates an array of chars, and initializes it with a specific char.
+ *create_array - creates an array of 
+ *chars, and initializes it with a specific char.
  *@size: buffer size
  *@c: char to fill
  *Return: the first buffer
