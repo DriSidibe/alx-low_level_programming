63eba79e4d59e4ad0c891c27b6c6ec38b72f0999
DriSidibe <dsidibe653@gmail.com>
2022-10-10 04:40:13 -0700

    commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef __STRUCT__
+#define __STRUCT__
+
+/**
+ * struct dog - Define a new type struct dog with
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+
+#endif
