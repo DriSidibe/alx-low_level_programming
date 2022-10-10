8f2618820bc61e6d614fb820ffb3a196d159e6d1
DriSidibe <dsidibe653@gmail.com>
2022-10-10 12:38:30 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,22 @@
-#ifndef __STRUCT__
-#define __STRUCT__
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - Define a new type struct dog with
- * @name: the name of the dog
- * @age: the age of the dog
- * @owner: the owner of the dog
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
  */
-struct dog
+typedef struct dog
 {
-
 	char *name;
 	float age;
 	char *owner;
-
-}dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif
