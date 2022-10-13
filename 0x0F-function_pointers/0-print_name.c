f0c47a4fa9d0c53732162190496a287c2836c1b3
DriSidibe <dsidibe653@gmail.com>
2022-10-13 04:15:07 -0700

    commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints a name as func
+ * @name: name of the person
+ *@f: the name format
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	printf("%s\n", f(name));
+}
