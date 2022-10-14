796337c74a9140aba6bf209b20c504cb687d65b0
DriSidibe <dsidibe653@gmail.com>
2022-10-14 03:59:59 -0700

    commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -20,7 +20,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
-		sum += (int)var_args(args, double);
+		sum += var_args(args, int);
 
 	va_end(args);
 
