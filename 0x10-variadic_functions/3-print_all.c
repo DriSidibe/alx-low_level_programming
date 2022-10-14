976561b9cb73374088da9989de6914651db2e152
DriSidibe <dsidibe653@gmail.com>
2022-10-14 04:31:55 -0700

    commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,58 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ *print_all - prints any argument of the function
+ *@format: list of types of arguments passed to the function
+ *
+ *Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	int  i;
+	char *sval, *sep;
+
+	va_list args;
+
+	va_start(args, format);
+
+	i = 0;
+
+	while (format != NULL && format[i])
+	{
+		sep = "";
+
+		if (format[i + 1])
+		{
+			sep = ", ";
+		}
+
+		switch (format[i])
+		{
+		case 'i':
+			printf("%i%s", va_arg(args, int), sep);
+			break;
+		case 'c':
+			printf("%c%s", va_arg(args, int), sep);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(args, double), sep);
+			break;
+		case 's':
+			sval = va_arg(args, char *);
+
+			if (sval == NULL || sval[0] == '\0')
+			{
+				sval = "(nil)";
+			}
+
+			printf("%s%s", sval, sep);
+			break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,4 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char 0-sum_them_all.c 1-print_numbers.c 2-print_strings.c README.md variadic_functions.h const format, ...);
