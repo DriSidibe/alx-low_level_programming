4badb73fd3501797c8f8faa97f33419096605780
DriSidibe <dsidibe653@gmail.com>
2022-10-21 04:21:21 -0700

    commit

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -1,1 +1,22 @@
+#ifndef __MAIN_H_
+#define __MAIN_H_
+
+
+/**
+ * struct list_t - singly linked list
+ * @str: ...
+ * @len: length
+ * @next: ...
+ *
+ * Description: ...
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+}list_t;
+
 size_t print_list(const list_t *h);
+
+#endif
