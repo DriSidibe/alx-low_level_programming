b2566f3d05fd68671131296ecc441904f10b8fec
DriSidibe <dsidibe653@gmail.com>
2022-10-25 03:39:27 -0700

    commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,48 +10,33 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *tmp_node;
-	listint_t *node;
+	listint_t *tmp_node, *node;
 
 	tmp_node = *head;
 
 	if (tmp_node == NULL)
 		return (-1);
 
-
 	if (index == 0)
 	{
-
 		*head = tmp_node->next;
-
 		free(tmp_node);
-
 		return (1);
-
 	}
-
 
 	i = 1;
 
 	while (i < index)
 	{
-
 		tmp_node = tmp_node->next;
 
 		if (tmp_node == NULL)
 			return (-1);
-
 		i++;
-
 	}
 
-
 	node = tmp_node->next;
-
 	tmp_node->next = node->next;
-
 	free(node);
-
 	return (1);
-
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,28 +10,19 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-
 	unsigned int i;
-	listint_t *tmp_node;
-	listint_t *new_node;
-
+	listint_t *tmp_node, *new_node;
 
 	if (head == NULL)
 		return (NULL);
 
-
 	if (idx == 0)
 	{
-
 		tmp_node = add_nodeint(head, n);
-
 		return (tmp_node);
-
 	}
 
-
 	tmp_node = *head;
-
 	i = 1;
 
 	while (i < idx)  /* travers till idx and get the node to insert */
@@ -40,28 +31,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 		if (tmp_node == NULL)
 			return (NULL);
-
 		i++;
-
 	}
-
 
 	new_node = malloc(sizeof(listint_t));  /* allocate new node */
 
 	if (new_node == NULL)
 		return (NULL);
 
-
 	new_node->n = n;                      /* set value in new node */
-
 	new_node->next = tmp_node->next; /* link on the right */
-
 	tmp_node->next = new_node;   /* link on the left side */
-
 	return (new_node);
-
 }
-
 
 
 /**
@@ -81,11 +63,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-
 	new_node->next = *head;
-
 	*head = new_node;
-
 	return (new_node);
-
 }
