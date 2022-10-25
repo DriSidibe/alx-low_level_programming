88f981c8fae737c20d153f8962964675a9154112
DriSidibe <dsidibe653@gmail.com>
2022-10-25 02:57:58 -0700

    commit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -12,7 +11,7 @@ size_t print_listint(const listint_t *h)
 {
 	int len = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		len++;
 		h = h->next;
