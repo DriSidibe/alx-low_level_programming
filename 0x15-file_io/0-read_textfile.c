f9eab832bfcb0e6eb9c72fc02e7c5fb2e073a0d5
DriSidibe <dsidibe653@gmail.com>
2022-11-04 10:49:42 -0700

    commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *read_textfile - reads a text file and prints it to the POSIX standard output.
+ *@filename: the name of the file
+ *@letters: number of letters
+ *Return: the size of the input string
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	int file;
+	char str[letters - 1];
+
+	file = open(filename, 0_RDONLY);
+
+	if (file == -1)
+		return (0);
+
+	read(file, str, letters);
+
+	str[letters] = '\n';
+
+	for(i = 0; i < letters; letters++)
+		_putchar(str[i]);
+
+	close(file);
+
+	return ((ssize_t)letters);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,7 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
