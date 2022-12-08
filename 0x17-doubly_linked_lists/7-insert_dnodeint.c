e4c9db9d6ad03e8cef4a8d3f22cf3945bec5a1e3
DriSidibe <dsidibe653@gmail.com>
2022-12-08 16:48:22 +0000

    commit

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: the double linked list head
+ * @idx: the index
+ * @n: the data
+ *
+ * Return: inserts a new node at a given position.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *prev = *h;
+	dlistint_t *new = NULL;
+	dlistint_t *cursor = *h;
+	unsigned int i = 0;
+	unsigned int len = 0;
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	while (cursor != NULL)
+	{
+		cursor = cursor->next;
+		len++;
+	}
+
+	if (idx >= len)
+		return (NULL);
+
+	cursor = *h;
+
+	while (i != idx)
+	{
+		cursor = cursor->next;
+		prev = cursor->prev;
+		i++;
+	}
+
+	prev->next = new;
+	new->next = cursor;
+	new->prev = prev;
+	cursor->prev = new;
+	new->n = n;
+
+	return (new);
+}
