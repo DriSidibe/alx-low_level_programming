322a13f15137f8aa23edc8efd279d663b1fedc66
DriSidibe <dsidibe653@gmail.com>
2022-12-12 16:50:37 +0000

    commit

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,54 @@
+/**
+ *op_add - adds 2 numbers
+ *@a: num 1
+ *@b: num2
+ *Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - substracts 2 numbers
+ *@a: num 1
+ *@b: num 2
+ *Return: difference of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - multiplies two numbers
+ *@a: num 1
+ *@b: num 2
+ *Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - divides two numbers
+ *@a: num 1
+ *@b: num 2
+ *Return: result of the division of a by b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *op_mod - displays remainder after division
+ *@a: num 1
+ *@b: num 2
+ *Return: remainder of the division of a by b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
