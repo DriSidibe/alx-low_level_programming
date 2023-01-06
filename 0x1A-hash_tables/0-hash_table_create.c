7d25979f5f6eb00cfb13b49668b3bd56449a57c8
DriSidibe <dsidibe653@gmail.com>
2023-01-06 21:02:43 +0000

    commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,17 @@
  *
  *Return: the hash table head
  */
-hash_table_t *hash_table_create(unsigned long int size){
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 
-    hash_table_t* table = (hash_table_t*) malloc (sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
 
-    table->size = size;
-    table->array = (hash_node_t**) malloc(sizeof(hash_node_t) * size);
-	if (table == NULL || table->array == NULL)
-		return NULL;
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+	if (table->array == NULL)
+		return (NULL);
 
-    return table;
+	return (table);
 }
