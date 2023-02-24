cf05a13ae44a47c9d9fc6129fb61d7d81827e5e4
DriSidibe <dsidibe653@gmail.com>
2023-02-24 21:12:50 +0000

    commit

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module contains the function island_perimeter"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid:
+    - grid is a list of list of integers:
+    - 0 represents a water zone
+    - 1 represents a land zone
+    - One cell is a square with side length 1
+    - Grid cells are connected horizontally/vertically (not diagonally).
+    - Grid is rectangular, width and height don’t exceed 100
+    """
+    land = 0
+    water = 0
+    grid_y = len(grid)
+    grid_x = len(grid[0])
+    # print("Grid y:{} and x:{}".format(grid_y, grid_x))
+    for y in range(grid_y):
+        for x in range(grid_x):
+            if grid[y][x] == 1:
+                land += 1
+                # only count if it has any right water or down water
+                if (y > 0 and grid[y - 1][x] == 1):
+                    water += 1
+                if (x > 0 and grid[y][x - 1] == 1):
+                    water += 1
+    # every adjacent lands made two sides disappeared.
+    return land * 4 - water * 2
